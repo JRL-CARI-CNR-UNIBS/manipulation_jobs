4f7a064d5ddfe518399255a8ef68dd82dd9008f7
CARI JRL <cari@jrl.com>
2021-11-18 15:14:26 +0100

    cembre

--- manipulation_job_base/scripts/push_gripper.py
+++ manipulation_job_base/scripts/push_gripper.py
@@ -60,6 +60,7 @@ def main():
                 goal.release_condition=simple_touch_controller_msgs.msg.simpleTouchGoal.FORCE
             if current_state["release_condition"]=="None":
                 goal.release_condition=simple_touch_controller_msgs.msg.simpleTouchGoal.NONE
+                goal.release_condition=0.0
             if current_state["release_condition"]=="Position":
                 goal.release=current_state["release_position"]
                 goal.release_condition=simple_touch_controller_msgs.msg.simpleTouchGoal.POSITION
@@ -73,13 +74,20 @@ def main():
             else:
                 current_state_name=current_state["next_state_if_fail"]
 
+            req.start_configuration="trajectory_tracking"
+            try:
+                res=config_client(req)
+            except:
+                rospy.loginfo('no server')
+                current_state_name = "FAIL"
+                continue
 
         if (current_state["type"]=='Gripper'):
 
             rospy.loginfo(current_state_name+ " Gripper")
             req=manipulation_msgs.srv.JobExecutionRequest()
             gripper_client.wait_for_service()
-            property_id="pos_"+str(position)+"_force_"+str(current_state["force"])+"_vel_"+str(current_state["velocity"])
+            property_id="pos_"+str(current_state["position"])+"_force_"+str(current_state["force"])+"_vel_"+str(current_state["velocity"])
             print(property_id)
 
             req.property_id=property_id
@@ -98,12 +106,11 @@ def main():
             req.start_configuration="cartesian_position"
             try:
                 res=config_client(req)
-
             except:
                 rospy.loginfo('no server')
                 current_state_name = "FAIL"
                 continue
-
+            rospy.loginfo(current_state_name+ " waiting for server")
             move_client.wait_for_server()
 
             goal=relative_cartesian_controller_msgs.msg.RelativeMoveGoal()
@@ -135,7 +142,7 @@ def main():
             else:
                 position=current_state["position"]
                 orientation=current_state["orientation"]
-            
+
             goal.relative_pose.pose.position.x=position[0]
             goal.relative_pose.pose.position.y=position[1]
             goal.relative_pose.pose.position.z=position[2]
@@ -143,14 +150,14 @@ def main():
             goal.relative_pose.pose.orientation.y=orientation[1]
             goal.relative_pose.pose.orientation.z=orientation[2]
             goal.relative_pose.pose.orientation.w=orientation[3]
-            
+
             if "linear_velocity_m_s" in current_state:
                 goal.target_linear_velocity=current_state["linear_velocity_m_s"]
             elif "linear_velocity_mm_s" in current_state:
                 goal.target_linear_velocity=current_state["linear_velocity_mm_s"]/1000.0
             else:
                 goal.target_linear_velocity=0.250
-                
+
             if "angular_velocity_deg_s" in current_state:
                 goal.target_angular_velocity=current_state["angular_velocity_deg_s"]*math.pi/180.0
             elif "angular_velocity_rad_s" in current_state:
@@ -162,14 +169,24 @@ def main():
             rospy.loginfo(current_state_name+ " MOVE SEND GOAL")
             move_client.send_goal(goal)
             move_client.wait_for_result()
+            #rospy.sleep(15)
             move_result=move_client.get_result()
             if (move_result.error_code>=0):
                 current_state_name=current_state["next_state_if_success"]
             else:
+                print('error code ',move_result.error_code)
                 if "next_state_if_fail" in current_state:
                     current_state_name=current_state["next_state_if_fail"]
                 else:
                     current_state_name="FAIL"
+
+            req.start_configuration="trajectory_tracking"
+            try:
+                res=config_client(req)
+            except:
+                rospy.loginfo('no server')
+                current_state_name = "FAIL"
+                continue
 
     print(subjobs.keys())
 
